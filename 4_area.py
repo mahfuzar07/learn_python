3867a96002a46b99a0cf0e17d6381c8bec709642
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-06-21 22:49:15 +0600

    Create 4_area.py

---
+++ 4_area.py
@@ -0,0 +1,15 @@
+#Traiangle (Area = 1/2 * base * Vertical height)
+#Rectangle (Area = Width * height)
+#Square    (Area = Length of side ^2 )
+#Parallelogram (Area = base * Vertical height)
+#Trapezoid (Area = 1/2 (a.base + b.base) * Vertical height)
+#circle    (Area = Pie * radius ^2)
+#Ellipse   (Area = Pie * a.base * b.base )
+#Sector    (Area = 1/2 * Radius ^2 * angle in radians)
+
+base = float(input("Enter Base : "))
+height = float(input("Enter Vertical Height : "))
+
+area = 0.5 * base * height
+print("Traiangle Area: ",area )
+

02db24e3b92cf0914e4e05303da17221b519badc
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-06-21 22:49:11 +0600

    Create 3_variables.py

---
+++ 3_variables.py
@@ -0,0 +1,12 @@
+name =input("Enter your name :")
+age = int(input("Enter your age :"))
+result = float(input("Enter your result :"))
+
+print(" Your Information ")
+print("--------------------------------------------")
+print("New student name is "+ name)
+print(name + " lives in Dhaka ")
+print(name + " is curently ",age," years old ")
+print(name + " is scored ",result," in last exam ")
+
+

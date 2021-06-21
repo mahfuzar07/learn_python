9445327c433079f1c756a4cd6a231badcbfa744d
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-06-21 22:49:07 +0600

    Create 2_type_casting.py

---
+++ 2_type_casting.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter Second Number :"))
+
+result = num1 + num2
+print("The result is add ",result)
+
+result = num1 - num2
+print("The result is sub  ",result)
+
+result = num1 * num2
+print("The result is mul ",result)

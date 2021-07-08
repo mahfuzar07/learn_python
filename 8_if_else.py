066a501b8a3855124cfe72b43d6a1631578800e9
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-07-09 01:56:25 +0600

    Update 8_if_else.py

--- 8_if_else.py
+++ 8_if_else.py
@@ -1,6 +1,15 @@
-mark = 70
-if mark >= 33:
-   print("pass")
+letter = input("Enter your Letter: ")
+letter = letter.lower()
+if letter == "a":
+   print("vowel")
+elif letter == "e":
+    print("vowel")
+elif letter == "i":
+    print("vowel")
+elif letter == "o":
+    print("vowel")
+elif letter == "u":
+    print("vowel")
 
 else:
-   print("fail")
+    print("Constant")

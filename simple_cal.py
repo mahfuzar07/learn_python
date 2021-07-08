e14b223cc87843d427462b73caceaa08d0ee1db9
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-07-09 01:57:35 +0600

    Create simple_cal.py

---
+++ simple_cal.py
@@ -0,0 +1,6 @@
+product1 = input().split(" ")
+product2 = input().split(" ")
+cod1, unit1, price1 = product1 
+cod2, unit2, price2 = product2 
+amount = (int(unit1) * float(price1)) +(int(unit2) * float(price2))
+print(f'VALOR A PAGAR: R$ {amount:.2f}') 
